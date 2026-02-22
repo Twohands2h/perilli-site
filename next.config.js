bf4b610c9ea98b9add9697193595767bbf0b30a9
Piero Perilli <piero.@Host-003.homenet.telecomitalia.it>
2026-02-22 17:30:23 +0100

    fix: add cdn.sanity.io to Next.js image domains

--- next.config.js
+++ next.config.js
@@ -13,6 +13,10 @@ const nextConfig = {
         protocol: 'https',
         hostname: '**.vimeocdn.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
     ],
   },
 };
