7eb761be6e8ecc6380c640db07836d2b48cc8bbe
Piero Perilli <piero.@Mac.homenet.telecomitalia.it>
2026-02-24 22:04:47 +0100

    fix: rimuovi import TestimonialsSection inutilizzato

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -5,7 +5,6 @@ import PortfolioSection from '@/components/sections/PortfolioSection';
 import PillarsSection from '@/components/sections/PillarsSection';
 import MethodSection from '@/components/sections/MethodSection';
 import SocialProofSection from '@/components/sections/SocialProofSection';
-import TestimonialsSection from '@/components/sections/TestimonialsSection';
 import CTASection from '@/components/sections/CTASection';
 
 function WebSiteSchema() {
