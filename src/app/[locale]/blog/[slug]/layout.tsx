f3e949b57da76bb5663cdbd92e1ee3dcc8985b09
Piero Perilli <piero.@Host-009.homenet.telecomitalia.it>
2026-03-04 12:19:41 +0100

    fix: blog posts use article thumbnail as og:image instead of generic site image

--- src/app/[locale]/blog/[slug]/layout.tsx
+++ src/app/[locale]/blog/[slug]/layout.tsx
@@ -14,11 +14,24 @@ export async function generateMetadata({ params }: { params: { locale: string; s
 
     const isIt = params.locale === 'it';
 
+    const ogBase = getPageOpenGraph(`/blog/${post.slug}`, params.locale);
+
     return {
         title: `${isIt ? post.titleIt : post.titleEn} | Piero Perilli`,
         description: isIt ? post.excerptIt : post.excerptEn,
         alternates: getPageAlternates(`/blog/${post.slug}`, params.locale),
-        openGraph: getPageOpenGraph(`/blog/${post.slug}`, params.locale),
+        openGraph: {
+            ...ogBase,
+            type: 'article',
+            images: [
+                {
+                    url: `https://pieroperilli.com${post.thumbnail}`,
+                    width: 1200,
+                    height: 630,
+                    alt: isIt ? post.thumbnailAlt.it : post.thumbnailAlt.en,
+                },
+            ],
+        },
     };
 }
 
